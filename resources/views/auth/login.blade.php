f24d847985805f1a0a4ab037494bae7d743fa76c
debuggingbytes <94572956+debuggingbytes@users.noreply.github.com>
2022-05-18 08:22:26 -0600

    Changed layout to hillside login and not jetstream

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,55 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Hillside Equipment Login</title>
+  <!-- Bootstrap Framework -->
+  <!-- Stylesheet -->
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <link rel="stylesheet" href="{{asset('files/style.css')}}">
+  <!-- Font awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+  <!-- jQuery -->
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
-        <x-jet-validation-errors class="mb-4" />
+</head>
+<body class="bg-light">
+  <main class="container vh-100 position-relative">
+    {{-- Absolute center of page --}}
+    <div class="abs-center">
+      <div class="row d-block">
+        <div class="col-md-10 mx-auto">
+          <img src="{{ asset('images/logo/hillside-full-transparent.png') }}" class="img-fluid w-100 mb-5">
+          <hr class="hr-line w-100">
+        </div>
+      </div>
+      <div class="row d-block">
+        <div class="col-md-10 mx-auto">
+          {!! Form::open(['route' => 'login', 'method' => 'post']) !!}
+          <div class="mb-3">
+            <label for="email" class="form-label">Email:</label>
+            <input type="text" name="email" id="email" class="form-control" required autofocus>
+          </div>
+          <div class="mb-3">
+            <label for="password" class="form-label">Password:</label>
+            <input type="password" name="password" id="password" class="form-control" required autocomplete="current-password">
+          </div>
+          <div class="mb-3">
+            <label for="remember" class="form-label me-2">Remember Me:</label>
+            <input type="checkbox" name="remember" id="remember">
+          </div>
+          <div class="mb-3">
+            <input type="submit" value="Submit" class='btn btn-info w-100'>
+          </div>
+          {!! Form::close() !!}
+        </div>
+      </div>
+    </div>
+  </main>
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+  
+</body>
+</html>
